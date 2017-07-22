e40fe799bef582297acaf0784014cf13e167b687
Basile Starynkevitch <basile@starynkevitch.net>
2017-07-22 21:10:05 +0200

    using gtk_init_with_args

--- main_BM.c
+++ main_BM.c
@@ -11,13 +11,41 @@ abort_BM (void)
 
 
 ////////////////////////////////////////////////////////////////
+char *load_dir_bm;
+char *dump_dir_bm;
+bool batch_bm;
+const GOptionEntry optab[] = {
+  {.long_name = "load",.short_name = 'l',
+   .flags = G_OPTION_FLAG_NONE,
+   .arg = G_OPTION_ARG_FILENAME,
+   .arg_data = &load_dir_bm,
+   .description = "load directory DIR (default is .)",
+   .arg_description = "DIR"},
+  {.long_name = "dump",.short_name = 'd',
+   .flags = G_OPTION_FLAG_NONE,
+   .arg = G_OPTION_ARG_FILENAME,
+   .arg_data = &dump_dir_bm,
+   .description = "dump directory DIR",
+   .arg_description = "DIR"},
+  {.long_name = "batch",.short_name = 'B',
+   .flags = G_OPTION_FLAG_NONE,
+   .arg = G_OPTION_ARG_NONE,
+   .arg_data = &batch_bm,
+   .description = "disable GUI"},
+  {}
+};
+
 int
 main (int argc, char **argv)
 {
   /// should actually use gtk_init_with_args so define some
   /// GOptionEntry array
-  bool batch = false;
-  for (int ix = 1; ix < argc; ix++)
-    if (!strcmp (argv[ix], "-B") || !strcmp (argv[ix], "--batch"))
-      batch = true;
+  GError *err = NULL;
+  bool guiok = gtk_init_with_args (&argc, &argv, " - The bismon program",
+                                   optab, NULL, &err);
+  if (!guiok && !batch_bm)
+    {
+      fprintf (stderr, "%s forcing batch (gtk init failed)\n", argv[0]);
+      batch_bm = true;
+    };
 }                               /* end main */
