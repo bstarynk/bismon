ae5559d056fcff19df3daf0df409a272cb6edf83
Basile Starynkevitch <basile@starynkevitch.net>
2017-08-08 22:03:03 +0200

    destroy of mainwin & app->quit works

--- gui_BM.c
+++ gui_BM.c
@@ -1,13 +1,62 @@
 // file gui_BM.c
 #include "bismon.h"
 
+GtkWidget *mainwin_BM;
+
 const char *
 gobjectclassnamedbg_BM (GObject * ptr)
 {
   if (!ptr)
     return "*nil*";
   return G_OBJECT_CLASS_NAME (G_OBJECT_GET_CLASS (ptr));
-}
+}                               /* end gobjectclassnamedbg_BM */
+
+void quit_BM (void);
+void
+quit_BM (void)
+{
+  //  printf ("quit_BM\n");
+  GtkWidget *quitdialog =       //
+    gtk_message_dialog_new_with_markup  //
+    (GTK_WINDOW (mainwin_BM),
+     GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+     GTK_MESSAGE_QUESTION,
+     GTK_BUTTONS_OK_CANCEL,
+     "Quit without dumping?");
+  int resp = gtk_dialog_run (GTK_DIALOG (quitdialog));
+  if (resp == GTK_RESPONSE_OK)
+    gtk_main_quit ();
+  gtk_widget_destroy (quitdialog);
+}                               /* end quit_BM */
+
+bool deletemainwin_BM (GtkWidget *, GdkEvent *, gpointer);
+bool
+deletemainwin_BM (GtkWidget * widget __attribute__ ((unused)),
+                  GdkEvent * ev __attribute__ ((unused)),
+                  gpointer data __attribute__ ((unused)))
+{
+  // printf ("deletemainwin_BM\n");
+  GtkWidget *quitdialog =       //
+    gtk_message_dialog_new_with_markup  //
+    (GTK_WINDOW (mainwin_BM),
+     GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+     GTK_MESSAGE_QUESTION,
+     GTK_BUTTONS_OK_CANCEL,
+     "Quit (after delete) without dumping?");
+  int resp = gtk_dialog_run (GTK_DIALOG (quitdialog));
+  if (resp == GTK_RESPONSE_OK)
+    {
+      gtk_main_quit ();
+      gtk_widget_destroy (quitdialog);
+      return false;             // to let the window be destroyed
+    }
+  else
+    {
+      gtk_widget_destroy (quitdialog);
+      return true;
+    }
+}                               /* end deletemainwin_BM */
+
 
 void
 initialize_gui_BM (const char *builderfile)
@@ -15,22 +64,31 @@ initialize_gui_BM (const char *builderfile)
   if (!builderfile)
     builderfile = "bismon.ui";
   GtkBuilder *bld = gtk_builder_new_from_file (builderfile);
-  GtkWidget *mainwin = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  //gtk_builder_add_callback_symbols (bld, "quitaction_BM", quit_BM, NULL);
+  mainwin_BM = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   GtkWidget *mainvbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
-  gtk_container_add (GTK_CONTAINER (mainwin), mainvbox);
+  gtk_container_add (GTK_CONTAINER (mainwin_BM), mainvbox);
   GtkWidget *mainmenubar = gtk_menu_bar_new ();
   gtk_box_pack_end (GTK_BOX (mainvbox), mainmenubar, /*expand= */ false,
                     /*fill= */ false, 4);
+  GtkWidget *appquit =
+    GTK_WIDGET (gtk_builder_get_object (bld, "appquit_id"));
+  g_signal_connect (appquit, "activate", quit_BM, NULL);
+  g_signal_connect (mainwin_BM, "delete-event", (GCallback) deletemainwin_BM,
+                    NULL);
   GtkWidget *appmenu =
-    GTK_WIDGET (gtk_builder_get_object (bld, "appmenu_id"));
+    GTK_WIDGET (gtk_builder_get_object (bld, "menuapp_id"));
   assert (GTK_IS_WIDGET (appmenu));
   gtk_menu_shell_append (GTK_MENU_SHELL (mainmenubar), appmenu);
   GtkWidget *editmenu =
-    GTK_WIDGET (gtk_builder_get_object (bld, "editmenu_id"));
+    GTK_WIDGET (gtk_builder_get_object (bld, "menuedit_id"));
   assert (GTK_IS_WIDGET (editmenu));
   gtk_menu_shell_append (GTK_MENU_SHELL (mainmenubar), editmenu);
-  gtk_window_set_title (GTK_WINDOW (mainwin), "bismon");
+  GtkWidget *sep1 = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
+  gtk_box_pack_end (GTK_BOX (mainvbox), sep1, /*expand= */ false,
+                    /*fill= */ false, 4);
+  gtk_window_set_title (GTK_WINDOW (mainwin_BM), "bismon");
   //GTK_WINDOW (gtk_builder_get_object (bld, "mainwin_id"));
-  gtk_window_set_default_size (GTK_WINDOW (mainwin), 560, 430);
-  gtk_widget_show_all (GTK_WIDGET (mainwin));
+  gtk_window_set_default_size (GTK_WINDOW (mainwin_BM), 560, 430);
+  gtk_widget_show_all (GTK_WIDGET (mainwin_BM));
 }                               /* end initialize_gui_BM */
