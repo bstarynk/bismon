2ee471b9efd8cf0ff4c170c907d8aa5a56006b91
Basile Starynkevitch <basile@starynkevitch.net>
2017-08-09 10:23:50 +0200

    regenerate _bm_global.h

--- _bm_global.h
+++ _bm_global.h
@@ -1,14 +1,14 @@
-// file _bm_global.h
-// should be generated
-
+// generated file for globals _bm_global.h
 #ifndef HAS_GLOBAL_BM
 #error missing HAS_GLOBAL_BM
 #endif
 
 #undef BM_NB_GLOBAL
 #define BM_NB_GLOBAL 2
-
-HAS_GLOBAL_BM(varglob)
 HAS_GLOBAL_BM(focusobj)
+HAS_GLOBAL_BM(varglob)
 
 #undef HAS_GLOBAL_BM
+
+
+// end of generated file _bm_global.h
