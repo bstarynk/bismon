fe1c90c591932b04a4f74734ca851f1dc84829c1
Basile Starynkevitch <basile@starynkevitch.net>
2017-10-01 12:40:03 +0200

    more in  prepare_routine:basiclo_function _5mnsT1wsdWs_2Qnqsf3wqaP

--- gencode_BM.c
+++ gencode_BM.c
@@ -6,11 +6,12 @@
 extern objrout_sigBM ROUTINEOBJNAME_BM (_5mnsT1wsdWs_2Qnqsf3wqaP);
 
 value_tyBM
-ROUTINEOBJNAME_BM (_5mnsT1wsdWs_2Qnqsf3wqaP) // prepare_routine:basiclo_function
+ROUTINEOBJNAME_BM (_5mnsT1wsdWs_2Qnqsf3wqaP)    // prepare_routine:basiclo_function
 (const closure_tyBM * clos,
-struct stackframe_stBM * stkf,
-const value_tyBM arg1,
-const value_tyBM arg2, const value_tyBM arg3, const quasinode_tyBM * restargs)
+ struct stackframe_stBM * stkf,
+ const value_tyBM arg1,
+ const value_tyBM arg2, const value_tyBM arg3,
+ const quasinode_tyBM * restargs)
 {
   enum
   {
@@ -20,5 +21,37 @@ const value_tyBM arg2, const value_tyBM arg3, const quasinode_tyBM * restargs)
     constix_constants,
     constix__LAST
   };
-  assert (!clos || isclosure_BM ((const value_tyBM) clos));
-}                               /* end  prepare_routine:basiclo_function _8kMqlEugRVW_7DgwjR4CBbP */
+  LOCALFRAME_BM ( /*prev: */ stkf, /*descr: */ NULL,
+                 const closure_tyBM * clos;
+                 objectval_tyBM * recv;
+    );
+  assert (isclosure_BM ((const value_tyBM) clos));
+  _.clos = clos;
+  const objectval_tyBM *closconn = closureconn_BM ((const value_tyBM) clos);
+  assert (closconn != NULL);
+  const node_tyBM *constnod = nodecast_BM (closconn->ob_data);
+  /*** constnod is
+      * const (arguments block locals constants)
+   ***/
+  assert (isnode_BM ((const value_tyBM) constnod)
+          && nodewidth_BM ((const value_tyBM) constnod) == constix__LAST
+          && valhash_BM ((const value_tyBM) constnod) == 4099148514);
+  const objectval_tyBM *k_arguments =   //
+    objectcast_BM (nodenthson_BM
+                   ((const value_tyBM) constnod, constix_arguments));
+  const objectval_tyBM *k_block =       //
+    objectcast_BM (nodenthson_BM
+                   ((const value_tyBM) constnod, constix_block));
+  const objectval_tyBM *k_locals =      //
+    objectcast_BM (nodenthson_BM
+                   ((const value_tyBM) constnod, constix_locals));
+  const objectval_tyBM *k_constants =   //
+    objectcast_BM (nodenthson_BM
+                   ((const value_tyBM) constnod, constix_constants));
+  _.recv = objectcast_BM (arg1);
+  DBGPRINTF_BM
+    ("start prepare_routine:basiclo_function _5mnsT1wsdWs_2Qnqsf3wqaP recv=%s\n",
+     objectdbg_BM (_.recv));
+#warning incomplete  prepare_routine:basiclo_function _5mnsT1wsdWs_2Qnqsf3wqaP
+  return NULL;
+}                               /* end  prepare_routine:basiclo_function _5mnsT1wsdWs_2Qnqsf3wqaP */
