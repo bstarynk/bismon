34d7d93c119bef297c4adfbb43b8766f407e1563
Basile Starynkevitch <basile@starynkevitch.net>
2017-10-30 10:00:08 +0100

    added _0UqUZ7mQGG5_69QlCmcSwTi foreach_in_tuple(args curarg curargix args ...)  into prepare_routine°basiclo_function

--- gencode_BM.c
+++ gencode_BM.c
@@ -82,7 +82,7 @@ ROUTINEOBJNAME_BM (_5mnsT1wsdWs_2Qnqsf3wqaP)    // prepare_routine:basiclo_funct
   objputattr_BM (_.routprepob, k_bindings, _.assocbind);
   /// assign _9YS82HkgghD_71BLsDAKRfg
   _.args = objgetattr_BM (_.recv, k_arguments);
-  /// foreach_in_tuple(args curarg curargix args ...)
+  /// foreach_in_tuple(args curarg curargix args ...) _0UqUZ7mQGG5_69QlCmcSwTi
 #warning incomplete  prepare_routine:basiclo_function _5mnsT1wsdWs_2Qnqsf3wqaP
   return NULL;
 }                               /* end  prepare_routine:basiclo_function _5mnsT1wsdWs_2Qnqsf3wqaP */
