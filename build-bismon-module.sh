59836fa114956416950c7bd3289bedd30c1fa48a
Basile Starynkevitch <basile@starynkevitch.net>
2018-04-27 16:03:30 +0200

    simplified build-bismon-module.sh

--- build-bismon-module.sh
+++ build-bismon-module.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -vx
+#!/bin/bash
 # file build-bismon-module.sh
 MODULEID=$1
 logger --id=$$ -s -t build-bismon-module -p user.debug starting moduleid $MODULEID in $(pwd) at $(date +%c)
-exec make -f $(dirname $0)/Makefile singlemodule MODULEID=$MODULEID
+exec make -f $(dirname $0)/Makefile --silent singlemodule MODULEID=$MODULEID
