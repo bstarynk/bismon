b9e89837064ca4881c1d033ca6d18f8b04d1f8bb
Basile Starynkevitch <basile@starynkevitch.net>
2018-04-29 07:11:35 +0200

    more verbose build-bismon-module.sh

--- build-bismon-module.sh
+++ build-bismon-module.sh
@@ -2,4 +2,12 @@
 # file build-bismon-module.sh
 MODULEID=$1
 logger --id=$$ -s -t build-bismon-module -p user.debug starting moduleid $MODULEID in $(pwd) at $(date +%c)
-exec make -f $(dirname $0)/Makefile --silent singlemodule MODULEID=$MODULEID
+make -f $(dirname $0)/Makefile --silent singlemodule MODULEID=$MODULEID
+makestatus=$?
+if [ $makestatus -ne 0 ]; then
+    logger --id=$$ -s -t build-bismon-module -p user.debug make for $MODULEID failed with $makestatus
+    exit $makestatus
+else
+    logger --id=$$ -s -t build-bismon-module -p user.debug make for $MODULEID completed
+fi
+
