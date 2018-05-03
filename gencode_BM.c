f2758e58e59af934313409f19b32176623a54def
Basile Starynkevitch <basile@starynkevitch.net>
2018-05-03 14:25:56 +0200

    start adding iterate_sequence_minifunc

--- gencode_BM.c
+++ gencode_BM.c
@@ -953,8 +953,8 @@ ROUTINEOBJNAME_BM (_23F5sZIfO5Y_5m9O2FPHdzX)    // miniscan_stmt°basiclo_exit
   WEAKASSERT_BM (_.routprepob);
   WEAKASSERT_BM (istaggedint_BM (arg3) && depth >= 0);
   WEAKASSERT_BM (_.fromob);
-#warning unimplemented _23F5sZIfO5Y_5m9O2FPHdzX routine
-  WEAKASSERT_BM (false && "unimplemented _23F5sZIfO5Y_5m9O2FPHdzX routine");
+#warning unimplemented miniscan_stmt°basiclo_exit _23F5sZIfO5Y_5m9O2FPHdzX 
+  WEAKASSERT_BM (false && "unimplemented miniscan_stmt°basiclo_exit _23F5sZIfO5Y_5m9O2FPHdzX ");
   LOCALRETURN_BM (_.resultv);
 }                               /* end miniscan_stmt°basiclo_exit  _23F5sZIfO5Y_5m9O2FPHdzX */
 
