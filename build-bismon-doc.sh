e44cdfa83175cede38142370165ddf72f7b51f92
Basile Starynkevitch <basile@starynkevitch.net>
2018-05-04 22:59:24 +0200

    empty bismon-doc

--- build-bismon-doc.sh
+++ build-bismon-doc.sh
@@ -15,9 +15,11 @@
 
 mkdir -p doc/generated/
 
+# generate the git tag
 git log --format=oneline -1 --abbrev=16 --abbrev-commit -q | awk '{printf "\\newcommand{\\mygitcommit}[0]{%s}\n", $1}' > doc/generated/git-commit.tex
 
-date +'\\newcommand{\\mydocdate[0]{%c}%n' > doc/generated/timestamp.tex
+# generate the dates
+date +'\\newcommand{\\mydoctimestamp[0]{%c}%n\\mydocdate[0]{%b %d, %Y}' > doc/generated/timestamp.tex
 
 for gscript in doc/genscripts/[0-9]*.sh ; do
     gbase="$(basename $gscript .sh)"
@@ -25,15 +27,23 @@ for gscript in doc/genscripts/[0-9]*.sh ; do
     $gscript > "doc/generated/$gbase.tex"
 done
 
+# generate vectorial images
 for svgfile in doc/images/*.svg ; do
     sbase="$(basename $svgfile .svg)"
     inkscape --without-gui --export-pdf="doc/generated/$sbase-fig.pdf" "$svgfile"
     inkscape --without-gui --export-eps="doc/generated/$sbase-fig.eps" "$svgfile"
 done
 
-
+# generate JPEG images
 for jpegfile in doc/images/*.jpeg ; do
     jbase="$(basename $jpegfile .jpeg)"
     convert "$jpegfile" "doc/generated/$jbase-img.pdf"
     convert "$jpegfile" "doc/generated/$jbase-img.eps"
 done
+
+cd doc
+pdflatex bismon-doc
+bibtex bismon-doc
+pdflatex bismon-doc
+makeindex bismon-doc
+pdflatex bismon-doc
