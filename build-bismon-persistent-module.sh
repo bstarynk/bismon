066716ff016e56160c6e1becf410fb91feb84598
Basile Starynkevitch <basile@starynkevitch.net>
2018-07-10 17:52:28 +0200

    add echo in build-bismon-persistent-module.sh

--- build-bismon-persistent-module.sh
+++ build-bismon-persistent-module.sh
@@ -2,6 +2,7 @@
 # file build-bismon-persistent-module.sh
 MODULEID=$1
 logger --id=$$ -s -t build-bismon-persistent-module -p user.debug starting moduleid $MODULEID in $(pwd) at $(date +%c)
+echo '@*@*@' $0 "$@" '::: in' $(pwd) > /dev/tty
 make -f $(dirname $0)/Makefile --silent singlemodule MODULEID=$MODULEID
 makestatus=$?
 if [ $makestatus -ne 0 ]; then
