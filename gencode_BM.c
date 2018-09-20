5a0f0e1797b5ace84f843d8ae02170439293c6fe
Basile Starynkevitch <basile@starynkevitch.net>
2018-09-20 14:32:30 +0200

    even more info rom miniscan_node_conn°basiclo_primitive for type mismatch

--- gencode_BM.c
+++ gencode_BM.c
@@ -2363,8 +2363,8 @@ ROUTINEOBJNAME_BM (_1vuSUudDrEr_9UjFr4Pcy8r)    // miniscan_node_conn°basiclo_p
       _.curcomptypob =
         miniscan_compatype_BM (_.curargctypob, _.curtypob, CURFRAME_BM);
       if (!_.curcomptypob)
-        FAILHERE (makenode3_BM
-                  (k_arguments, taggedint_BM (ix), _.cursonv, _.curtypob));
+        FAILHERE (makenode4_BM
+                  (k_arguments, taggedint_BM (ix), _.cursonv, _.curargctypob, _.curtypob));
     }
   DBGPRINTF_BM
     ("miniscan_node_conn°basiclo_primitive done expv=%s result connrestypob=%s",
