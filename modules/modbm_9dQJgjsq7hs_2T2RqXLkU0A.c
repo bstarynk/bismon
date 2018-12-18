ee3efe4173e1a378c172835140ab229f4f1962fd
Basile Starynkevitch <basile@starynkevitch.net>
2018-12-18 07:24:36 +0100

    adding webhtml_module & emithtml_block

---
+++ modules/modbm_9dQJgjsq7hs_2T2RqXLkU0A.c
@@ -0,0 +1,152 @@
+// generated persistent module webhtml_module
+// in file modbm_9dQJgjsq7hs_2T2RqXLkU0A.c -- DONT EDIT
+// this generated code is GPLv3+ licensed.
+
+
+#include "bismon.h"
+
+#ifdef BISMON_MODID
+DECLARE_MODULE_BM(_9dQJgjsq7hs_2T2RqXLkU0A);
+extern moduleinit_sigBM modulinit_9dQJgjsq7hs_2T2RqXLkU0A_BM;
+#endif /*BISMON_MODID*/
+
+#ifdef BISMON_MOMD5
+const char modulmd5chksum_9dQJgjsq7hs_2T2RqXLkU0A_BM[] = BISMON_MOMD5;
+#endif /*BISMON_MOMD5*/
+
+
+
+// declare 1 routines
+
+extern const char *const routid_9dQJgjsq7hs_2T2RqXLkU0A_BM[1+1];
+
+
+// no failure
+
+extern objrout_sigBM crout_5NH940lCHYJ_3nuRm3flnzc_BM; //#0 !emithtml_block (o_hblock o_emitctx o_strbuf v_depth)
+
+
+// declare 1 constants
+objectval_tyBM* constob_9dQJgjsq7hs_2T2RqXLkU0A_BM[1+1];
+const char* const constid_9dQJgjsq7hs_2T2RqXLkU0A_BM[1+1];
+
+
+// define 1 routines
+
+
+value_tyBM crout_5NH940lCHYJ_3nuRm3flnzc_BM //#0
+ (struct stackframe_stBM* stkf, //
+//!emithtml_block (o_hblock o_emitctx o_strbuf v_depth)
+  // 4 arguments
+  const value_tyBM arg0,  // o_hblock
+  const value_tyBM arg1,  // o_emitctx
+  const value_tyBM arg2,  // o_strbuf
+  const value_tyBM arg3,  // v_depth
+  const quasinode_tyBM * restargs_ __attribute__((unused)))
+{ // start of _5NH940lCHYJ_3nuRm3flnzc
+  struct frame_5NH940lCHYJ_3nuRm3flnzc_BMst {
+    typedhead_tyBM stkfram_head;
+    struct stackframe_stBM *stkfram_prev;
+    objectval_tyBM *stkfram_descr;
+    value_tyBM stkfram_callfun;
+    int stkfram_state;
+    int stkfram_extra;
+    /// 5 local values:
+    value_tyBM v_3qw9bnATIhq_8loBraLeXzQ; // v_r
+    objectval_tyBM* o_41F1rKwGbaA_30OJWKsqNWy; // o_strbuf
+    value_tyBM v_4nJKFtMqlkA_1teQSqofNux; // v_depth
+    objectval_tyBM* o_6Ef2ORO1Mni_0tpNoRQGmrW; // o_hblock
+    objectval_tyBM* o_9nXRmTATjN4_5F9V7RvvgW0; // o_emitctx
+    /// 2 local numbers:
+    intptr_t n_2W7Ckt2WxEM_4NVYgEWE73B; // ii
+    intptr_t n_6Y20GNUgnSZ_4LkbrcRMU8f; // i_depth
+   } _;
+   memset (&_, 0, sizeof(struct frame_5NH940lCHYJ_3nuRm3flnzc_BMst));
+   _.stkfram_head.htyp = typayl_StackFrame_BM;
+   _.stkfram_head.hgc = 0;
+   _.stkfram_head.rlen = 5;
+   _.stkfram_descr = constob_9dQJgjsq7hs_2T2RqXLkU0A_BM[0] /*|_5NH940lCHYJ_3nuRm3flnzc*/;
+   ASSERT_BM (!stkf
+             || stkf->stkfram_pA.htyp == typayl_StackFrame_BM
+             || stkf->stkfram_pA.htyp == typayl_SpecialFrame_BM);
+   _.stkfram_prev = stkf;
+   // fetch 4 arguments:
+   _.o_6Ef2ORO1Mni_0tpNoRQGmrW = objectcast_BM (arg0); // o_hblock
+   _.o_9nXRmTATjN4_5F9V7RvvgW0 = objectcast_BM (arg1); // o_emitctx
+   _.o_41F1rKwGbaA_30OJWKsqNWy = objectcast_BM (arg2); // o_strbuf
+   _.v_4nJKFtMqlkA_1teQSqofNux = arg3; // v_depth
+   // fetched 4 arguments.
+   // no closed values in _5NH940lCHYJ_3nuRm3flnzc.
+   // routine _5NH940lCHYJ_3nuRm3flnzc body:
+
+startblock__86w0oglm27k_4ULVduuJiC6: __attribute__((unused));
+{ /* +block _86w0oglm27k_4ULVduuJiC6 */ 
+ /*i_depth:*/_.n_6Y20GNUgnSZ_4LkbrcRMU8f = //assign _7j8aQZXvf5R_12tuW3bNSTQ  
+ /* val_to_int */ getint_BM( /*v_depth:*/_.v_4nJKFtMqlkA_1teQSqofNux) ; 
+ 
+ 
+ 
+{ // run _9W3ifZwUp2j_3cTOMwHevI1 
+/*debug emithtml_block start o_hblock o_emitctx o_strbuf depth*/
+DBGPRINTF_BM("emithtml_block start €%.9s o_hblock=%s o_emitctx=%s o_strbuf=%s/L%d depth#%ld",
+ "_9W3ifZwUp2j_3cTOMwHevI1",
+ objectdbg_BM( /*o_hblock:*/_.o_6Ef2ORO1Mni_0tpNoRQGmrW),
+ objectdbg1_BM( /*o_emitctx:*/_.o_9nXRmTATjN4_5F9V7RvvgW0),
+ objectdbg2_BM( /*o_strbuf:*/_.o_41F1rKwGbaA_30OJWKsqNWy),
+ (int) objstrbufferlengthpayl_BM( /*o_strbuf:*/_.o_41F1rKwGbaA_30OJWKsqNWy),
+ (long int) ( /*i_depth:*/_.n_6Y20GNUgnSZ_4LkbrcRMU8f));; 
+} // end run _9W3ifZwUp2j_3cTOMwHevI1 
+ 
+/* !endingblock _86w0oglm27k_4ULVduuJiC6 */ 
+goto endblock__86w0oglm27k_4ULVduuJiC6; 
+endblock__86w0oglm27k_4ULVduuJiC6: ; 
+} /*-block _86w0oglm27k_4ULVduuJiC6 */
+
+ epilog_5NH940lCHYJ_3nuRm3flnzc: __attribute__ ((unused)); // routine _5NH940lCHYJ_3nuRm3flnzc epilogue:
+   if (stkf) stkf->stkfram_callfun = NULL;
+   return  /*v_r:*/_.v_3qw9bnATIhq_8loBraLeXzQ;
+} // end _5NH940lCHYJ_3nuRm3flnzc routine#0 _5NH940lCHYJ_3nuRm3flnzc
+
+
+
+
+// end of 1 generated routines
+
+
+// the constant ids for 1 constants:
+const char* const constid_9dQJgjsq7hs_2T2RqXLkU0A_BM[1+1] = {
+ /*0:*/
+ "_5NH940lCHYJ_3nuRm3flnzc", //! emithtml_block (o_hblock o_emitctx o_strbuf v_depth)
+ NULL}; // end 1 constant ids
+
+
+
+// the routine ids for 1 routines:
+const char* const routid_9dQJgjsq7hs_2T2RqXLkU0A_BM[1+1] = {
+  /*0:*/
+  "_5NH940lCHYJ_3nuRm3flnzc", //!emithtml_block (o_hblock o_emitctx o_strbuf v_depth)
+
+ NULL}; // end 1 routine ids
+
+
+
+// basiclo*module initialization
+#ifdef BISMON_MODID
+value_tyBM modulinit_9dQJgjsq7hs_2T2RqXLkU0A_BM
+  (struct stackframe_stBM *stkf, //
+   const value_tyBM arg1, //
+   const value_tyBM arg2, //
+   const value_tyBM arg3, //
+   void* dlh) {
+  return simple_module_initialize_BM(arg1, arg2, arg3, //
+        "_9dQJgjsq7hs_2T2RqXLkU0A",//
+       constob_9dQJgjsq7hs_2T2RqXLkU0A_BM, //
+       constid_9dQJgjsq7hs_2T2RqXLkU0A_BM, //
+       routid_9dQJgjsq7hs_2T2RqXLkU0A_BM, //
+       dlh, stkf);
+} // end modulinit_9dQJgjsq7hs_2T2RqXLkU0A_BM
+#endif /*BISMON_MODID*/
+
+
+
+// end of generated persistent module webhtml_module in file modbm_9dQJgjsq7hs_2T2RqXLkU0A.c
