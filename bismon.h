d58b9399aae0d1fdab5f8948c1e832cc5e9fc016
Basile Starynkevitch <basile@starynkevitch.net>
2019-01-01 07:22:53 +0100

    update copyright for 2019

--- bismon.h
+++ bismon.h
@@ -4,7 +4,7 @@
 
 /**
     BISMON 
-    Copyright © 2018 CEA (Commissariat à l'énergie atomique et aux énergies alternatives)
+    Copyright © 2018, 2019 CEA (Commissariat à l'énergie atomique et aux énergies alternatives)
     contributed by Basile Starynkevitch.
 
     This program is free software: you can redistribute it and/or modify
