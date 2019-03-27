2df64a596a004de2eea677d24ef7eff369980e9d
Basile Starynkevitch <basile@starynkevitch.net>
2019-03-27 18:35:36 +0100

    emit-src-from-webssesion-web_theme should become emit-href-from-webssesion-web_theme; and src= becomes href= for <link element; now the background is ivory

--- modules/modbm_9dQJgjsq7hs_2T2RqXLkU0A.c
+++ modules/modbm_9dQJgjsq7hs_2T2RqXLkU0A.c
@@ -545,7 +545,7 @@ DBGPRINTF_BM("emit-src-from-webssession-web_theme €%.9s o_theme=%s v_filepath=
   
 //+ #2/4 of lockobj _0DZ90FBZM3E_4v3rw8AWUQU ::  
 { // start out _8U4tim0A3AW_9Er3kDyv0dN to o_strbuf  
-/*out literal:*/ objstrbufferappendcstrpayl_BM ( /*o_strbuf:*/_.o_41F1rKwGbaA_30OJWKsqNWy, " src=\'");  
+/*out literal:*/ objstrbufferappendcstrpayl_BM ( /*o_strbuf:*/_.o_41F1rKwGbaA_30OJWKsqNWy, " href=\'");  
 /*out html_enc:*/ objstrbufferencodedhtmlpayl_BM ( /*o_strbuf:*/_.o_41F1rKwGbaA_30OJWKsqNWy, /*bytstring:*/bytstring_BM((const stringval_tyBM*)( /*v_filepath:*/_.v_7KirpiCMhmI_0IfcivXUJfQ)), -1);  
 /*out literal:*/ objstrbufferappendcstrpayl_BM ( /*o_strbuf:*/_.o_41F1rKwGbaA_30OJWKsqNWy, "\' ");  
 /*out string:*/ objstrbufferappendcstrpayl_BM ( /*o_strbuf:*/_.o_41F1rKwGbaA_30OJWKsqNWy, /*bytstring:*/bytstring_BM((const stringval_tyBM*)( /*v_str:*/_.v_167WczZjWtG_3FofhJxQWJi))); 
