d2fdf30b63a042a12d66d96fce714d852fcc66a7
Basile Starynkevitch <basile@starynkevitch.net>
2019-07-05 16:06:19 +0200

    DUMP FIGURE DOE NOT WORK: wrong doc/genscripts/001-extract-firsttestmodule-dump.sh

--- doc/genscripts/001-extract-firsttestmodule-dump.sh
+++ doc/genscripts/001-extract-firsttestmodule-dump.sh
@@ -5,4 +5,4 @@
 # object first_test_module in store2.bmon
 echo running genscripts/001-extract-firsttestmodule-dump.sh on $(wc -l ../store2.bmon) 1>&2
 
-sed -e '/!(_9oXtCgAbkqv_4y1xhhF5Nhz/,/!)_9oXtCgAbkqv_4y1xhhF5Nhz/p' -n ../store2.bmon 
+sed -e '/«_9oXtCgAbkqv_4y1xhhF5Nhz/,/»_9oXtCgAbkqv_4y1xhhF5Nhz/p' -n ../store2.bmon 
