d3fcfda0788f54f2b1a883ba1cd236b201b1077e
Basile Starynkevitch <basile@starynkevitch.net>
2019-11-18 11:26:08 +0100

    to be improved, but working with lualatex

---
+++ talks/Lamsade-21nov2019/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# file bismon/talks/Lamsade-21nov2019/build.sh
+# in http://github.com/bstarynk/bismon/
+lualatex Bismon-Starynkevitch-Lamsade-21nov2019
+lualatex Bismon-Starynkevitch-Lamsade-21nov2019
