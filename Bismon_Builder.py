6a807c0e1ed0bad4013677a36df552bbdcb02dd2
Basile Starynkevitch <basile@starynkevitch.net>
2020-09-30 10:49:54 +0200

    rename Bismon-Builder.py -> Bismon_Builder.py and improve it

---
+++ Bismon_Builder.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+## SPDX-License-Identifier: GPL-3.0-or-later
+## See https://github.com/bstarynk/bismon/
+## file Bismon_Builder.py
+##
+## BISMON
+## Copyright © 2020 CEA (Commissariat à l'énergie atomique et aux énergies alternatives)
+## contributed by Basile Starynkevitch.
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+##-
+## Contact me (Basile Starynkevitch) by email
+## basile@starynkevitch.net and/or basile.starynkevitch@cea.fr
+##
+import os                       # https://docs.python.org/3/library/os.html
+import subprocess               # https://docs.python.org/3/library/subprocess.html
+import argparse                 # https://docs.python.org/3/library/argparse.html
+import pygit2                   # https://www.pygit2.org/
+
+## we first need a working GCC 10 compiler, which could be overridden
+## with the $BISMON_CC environment variable
+global bismon_cc
+# https://stackoverflow.com/q/40697845/841108
+bismon_cc = os.getenv("BISMON_CC", "gcc")
+
+
+
+class Bismon_Builder:
+    def __init__(self):
+        from pygit2 import Repository
+        self.this_repo = Repository(".")
+        self.last_commit = self.this_repo.revparse_single('HEAD')
+        self.git_id = self.last_commit.oid
+    def __str__(self):
+        return 'Bismon_Builder<repo:{0}, git-commit:{1}>'.format(self.this_repo.path,
+                                                                 self.git_id.__str__()[:12])
+    def parse_program_arguments(self):
+        argparser = argparse.ArgumentParser(description=
+                                            '''the builder of the Bismon static source code analyzer
+(GPLv3+ licensed, on github.com/bstarynk/bismon/ ...)''',
+                                            epilog='''funded by chariotproject.eu
+ & decoder-project.eu, developed at www-list.cea.fr''',
+                                            allow_abbrev=True
+                                            )
+        argparser.add_argument('-V', "--version", help="show script version of %(prog)s",
+                               version=('Bismon_builder {0}' % (self.git_id.str())))
+
+
+builder = Bismon_Builder()
+
+builder.parse_program_arguments()
+
+print(builder)
+# bismon_cc_version= subprocess.Popen([bismon_cc, "--version"])
+# print("Bismon C compiler version is", bismon_cc_version)
+
+##-**************
+##-                           for Emacs...
+##- Local Variables: ;;
+##- compile-command: "pylint Bismon_uilder.py" ;;
+##- End: ;;
+##-**************/
