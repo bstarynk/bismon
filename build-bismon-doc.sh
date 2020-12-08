4aa5017cb6a224e8bdb5469d2be62efab72f5563
Basile Starynkevitch <basile@starynkevitch.net>
2020-12-08 11:20:33 +0100

    force Englice locale

--- build-bismon-doc.sh
+++ build-bismon-doc.sh
@@ -13,6 +13,19 @@
 # perhaps LEPTON: http://www.math.univ-paris13.fr/~lithiao/Lepton.html
 # perhaps ANT: http://mirror.hmc.edu/ctan/systems/ant/
 
+
+## force English locale
+export  LANG=en_US.UTF-8
+export  LC_IDENTIFICATION=en_US.UTF-8
+export  LC_NUMERIC=en_US.UTF-8
+export  LC_MEASUREMENT=en_US.UTF-8
+export  LC_TIME=en_US.UTF-8
+export  LC_PAPER=en_US.UTF-8
+export  LC_MONETARY=en_US.UTF-8
+export  LC_TELEPHONE=en_US.UTF-8
+export  LC_NAME=en_US.UTF-8
+export  LC_ADDRESS=en_US.UTF-8
+
 ## the optional program argument could be LaTeX or HeVeA or left empty
 docmode=$1
 
