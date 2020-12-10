821e49f4a00dd36278c3b8620373cef58aadb872
Basile Starynkevitch <basile@starynkevitch.net>
2020-12-10 10:06:21 +0100

    genscripts/straight-gcc-config.bash is checking that $GCC exists

--- doc/genscripts/straight-gcc-config.bash
+++ doc/genscripts/straight-gcc-config.bash
@@ -2,3 +2,8 @@
 # under github.com/bstarynk/bismon/
 # to be source-d by GNU bash scripts
 GCC=/usr/bin/gcc-10
+
+if [ ! -x "$GCC" ]; then
+    printf "BISMON straight-gcc-config with missing %s compiler\n" "$GCC" > /dev/stderr
+    exit 1
+fi
