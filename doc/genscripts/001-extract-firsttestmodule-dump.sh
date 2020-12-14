03c867ae9ec3e9a9ee5f111499228c8ea8688c4b
Basile Starynkevitch <basile@starynkevitch.net>
2020-12-14 14:55:40 +0100

    doc/genscripts/001-extract-firsttestmodule-dump.sh uses store§2.bmon

--- doc/genscripts/001-extract-firsttestmodule-dump.sh
+++ doc/genscripts/001-extract-firsttestmodule-dump.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # file doc/genscripts/001-extract-firsttestmodule-dump.sh
 
 printf "@@@BISMONDOC starting %s %s cwd %s:\n" "$0" "$*" $(pwd) > /dev/stderr
 
 # this script is running in doc/ directory and extracts the dump of
-# object first_test_module in store2.bmon
-echo running genscripts/001-extract-firsttestmodule-dump.sh on $(wc -l ../store2.bmon) 1>&2
+# object first_test_module in store§2.bmon
+echo running genscripts/001-extract-firsttestmodule-dump.sh on $(wc -l ../store§2.bmon) 1>&2
 
-sed -e '/«_9oXtCgAbkqv_4y1xhhF5Nhz/,/»_9oXtCgAbkqv_4y1xhhF5Nhz/p' -n ../store2.bmon 
+sed -e '/«_9oXtCgAbkqv_4y1xhhF5Nhz/,/»_9oXtCgAbkqv_4y1xhhF5Nhz/p' -n ../store§2.bmon
+## eof  doc/genscripts/001-extract-firsttestmodule-dump.sh
