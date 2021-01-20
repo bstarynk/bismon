b89cdc05288ec310cc3b38df3044f7f4faab11ba
Basile Starynkevitch <basile@starynkevitch.net>
2021-01-20 14:43:20 +0100

    adding .dir-locals.el for compiling under GNU emacs

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;; see https://emacs.stackexchange.com/a/706/2798
+((nil .
+  (compile-command . "./Build")))
