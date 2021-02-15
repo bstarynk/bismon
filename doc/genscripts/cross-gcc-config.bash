9e88be137314317e706e39202ec8900a65ea4e0e
Basile Starynkevitch <basile@starynkevitch.net>
2021-02-15 12:32:07 +0100

    improve doc/genscripts/cross-gcc-config.bash

--- doc/genscripts/cross-gcc-config.bash
+++ doc/genscripts/cross-gcc-config.bash
@@ -1,4 +1,5 @@
 # file genscripts/cross-gcc-config.bash
 # BISMON - see http://github.com/bstarynk/bismon/
 # to be source-d by scripts
+# on Debian or Ubuntu: install package gcc-10-mipsel-linux-gnu
 XGCC=/usr/bin/mipsel-linux-gnu-gcc-10
