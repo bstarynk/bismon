fe16b4e03e720d1b8feaff3cd828c0b699908cce
Basile Starynkevitch <basile@starynkevitch.net>
2021-04-21 18:18:48 +0200

    improving build-bismon-temporary-module.sh

--- build-bismon-temporary-module.sh
+++ build-bismon-temporary-module.sh
@@ -3,7 +3,9 @@
 # GPLv3+ licensed, see http://github.com/bstarynk/bismon/
 MODULEID=$1
 logger --id=$$ -s -t build-bismon-temporary-module -p user.debug starting moduleid $MODULEID in $(pwd) at $(date +%c)
-make -f $(dirname $0)/Makefile -e "VPATH=$(pwd):$(dirname $0)" modubin/tmpmobm$MODULEID.so
+make -f $(dirname $0)/GNUMakefile -e "VPATH=$(pwd):$(dirname $0)" \
+     BISMON_MODULE_ID=$MODULEID \
+     BISMON_MODULE_MD5SUM=$(/usr/bin/md5sum modules/modbm$MODULEID.c | cut -d\   -f1) modubin/tmpmobm$MODULEID.so
 makestatus=$?
 if [ $makestatus -ne 0 ]; then
     logger --id=$$ -s -t build-bismon-temporary-module -p user.debug make for $MODULEID failed with $makestatus
