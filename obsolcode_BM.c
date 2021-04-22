aebd68d8253e6ed69a15a405bc4fd038dc48cdf7
Basile Starynkevitch <basile@starynkevitch.net>
2021-04-22 13:29:13 +0200

    added 3 obsolete routines to obsolcode_BM.c, last being °°gtk_browse_data°assoc_object _0f1S28bCE26_3X6Vpz9lG4A, but make redump fails

--- obsolcode_BM.c
+++ obsolcode_BM.c
@@ -26,3 +26,64 @@
 ***/
 
 #include "bismon.h"
+
+////////////////
+// commandhandler#,insert _01zabIzVKNR_8AGQGMBkSd7
+
+extern objrout_sigBM ROUTINEOBJNAME_BM (_01zabIzVKNR_8AGQGMBkSd7);
+value_tyBM
+ROUTINEOBJNAME_BM (_01zabIzVKNR_8AGQGMBkSd7)    //commandhandler#,insert 
+(struct stackframe_stBM * stkf, //
+ const value_tyBM arg1,         // object
+ const value_tyBM arg2,         // rank 
+ const value_tyBM arg3,         // value
+ const value_tyBM arg4_ __attribute__((unused)),        //
+ const quasinode_tyBM * restargs_ __attribute__((unused)))
+{
+  LOCALFRAME_BM ( /*prev: */ stkf, /*descr: */ NULL,
+                 objectval_tyBM * recv;);
+  _.recv = arg1;
+  FATAL_BM
+    ("°°obsolete commandhandler#,insert  _01zabIzVKNR_8AGQGMBkSd7 recv %s",
+     objectdbg_BM (_.recv));
+}                               /* endobsol commandhandler#,insert  _01zabIzVKNR_8AGQGMBkSd7 */
+
+/// method to gtk_browse_data for class-s
+extern objrout_sigBM ROUTINEOBJNAME_BM (_09DxyieS5Wz_7pkad4F88FA);
+value_tyBM
+ROUTINEOBJNAME_BM (_09DxyieS5Wz_7pkad4F88FA)    // gtk_browse_data°class
+(struct stackframe_stBM * stkf, //
+ const value_tyBM arg1,         // the reciever
+ const value_tyBM arg2,         // the browse maxdepth
+ const value_tyBM arg3 __attribute__((unused)), //
+ const value_tyBM arg4 __attribute__((unused)), //
+ const quasinode_tyBM * restargs __attribute__((unused)))
+{
+  LOCALFRAME_BM ( /*prev: */ stkf, /*descr: */ NULL,
+                 objectval_tyBM * recv;);
+  _.recv = arg1;
+  FATAL_BM
+    ("°°obsolete gtk_browse_data for class-s _09DxyieS5Wz_7pkad4F88FA recv %s",
+     objectdbg_BM (_.recv));
+}                               /* endobsol gtk_browse_data for class-s _09DxyieS5Wz_7pkad4F88FA */
+
+
+//gtk_browse_data°assoc_object _0f1S28bCE26_3X6Vpz9lG4A
+extern objrout_sigBM ROUTINEOBJNAME_BM (_0f1S28bCE26_3X6Vpz9lG4A);
+
+value_tyBM
+ROUTINEOBJNAME_BM (_0f1S28bCE26_3X6Vpz9lG4A)    //gtk_browse_data°assoc_object
+(struct stackframe_stBM * stkf, //
+ const value_tyBM arg1,         //
+ const value_tyBM arg2,         //
+ const value_tyBM arg3_ __attribute__((unused)),        //
+ const value_tyBM arg4_ __attribute__((unused)),        //
+ const quasinode_tyBM * restargs_ __attribute__((unused)))
+{
+  LOCALFRAME_BM (stkf, /*descr: */ NULL, const objectval_tyBM * recv;
+    );
+  _.recv = arg1;
+  FATAL_BM
+    ("°°obsolete gtk_browse_data for assoc_object-s _0f1S28bCE26_3X6Vpz9lG4A recv %s",
+     objectdbg_BM (_.recv));
+}                               /* endobsol gtk_browse_data°assoc_object _0f1S28bCE26_3X6Vpz9lG4A */
