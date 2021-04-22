9dd10e214558e0ab52a58ad6422c13b8c4512845
Basile Starynkevitch <basile@starynkevitch.net>
2021-04-22 12:47:47 +0200

    improve and more robust persistent-module-build-bismon.sh check for C source module file

--- persistent-module-build-bismon.sh
+++ persistent-module-build-bismon.sh
@@ -10,6 +10,16 @@ logger --id=$$ -s -t persistent-module-build-bismon -p user.debug starting modul
 # remove --silent in make
 #echo '@*@!!@*@' in $(pwd) should-make "-f $(dirname $0)/Makefile -e "VPATH=$(pwd):$(dirname $0)" modubin/modbm$MODULEID.so" > /dev/tty
 
+if ! [ -f modules/modbm$MODULEID.c ]; then
+    logger --id=$$ -s -t persistent-module-build-bismon -p user.notice no source module file  modules/modbm$MODULEID.c in $(pwd)
+    exit 10
+fi
+
+if ! [ -f $(dirname $0)/GNUmakefile ]; then
+    logger --id=$$ -s -t persistent-module-build-bismon -p user.notice missing  $(dirname $0)/GNUmakefile  in $(pwd)
+    exit 11
+fi
+       
 make -f $(dirname $0)/GNUmakefile -e "VPATH=$(pwd):$(dirname $0)" \
      BISMON_MODULE_ID=$MODULEID \
      BISMON_MODULE_MD5SUM=$(/usr/bin/md5sum modules/modbm$MODULEID.c | cut -d\   -f1)
