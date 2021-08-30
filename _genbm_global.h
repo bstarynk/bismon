ce8e7a209782aa17ff819bc46089d13f58c4649f
Basile Starynkevitch <basile@starynkevitch.net>
2021-08-30 14:30:47 +0200

    redumped

--- _genbm_global.h
+++ _genbm_global.h
@@ -1,6 +1,6 @@
 // generated code file for globals _genbm_global.h
 // this generated file _genbm_global.h is GPLv3+ licensed
-// generated in 2021, Aug, 23
+// generated in 2021, Aug, 30
 #ifndef HAS_GLOBAL_BM
 #error missing HAS_GLOBAL_BM
 #endif
