b1fff011bb9d85f0537f22e2c8085530958dcb13
Basile Starynkevitch <basile@starynkevitch.net>
2022-02-22 10:38:30 +0100

    improve guigtk/indent.sh with -T types

--- guigtk/indent.sh
+++ guigtk/indent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-indent  --gnu-style --no-tabs --honour-newlines minilispbismon.h
-indent  --gnu-style --no-tabs --honour-newlines gtkbismonlisp.c
-indent  --gnu-style --no-tabs --honour-newlines minilisp.c
+indent  --gnu-style --no-tabs --honour-newlines -T Obj minilispbismon.h
+indent  --gnu-style --no-tabs --honour-newlines -T Obj -T GHashTable -T GSequence gtkbismonlisp.c
+indent  --gnu-style --no-tabs --honour-newlines -T Obj minilisp.c
 
