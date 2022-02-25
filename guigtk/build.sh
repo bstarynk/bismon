c8763bbc4113e6ce077aaae1d810749ea6330955
Basile Starynkevitch <basile@starynkevitch.net>
2022-02-25 10:56:35 +0100

    build.sh renames old executable to minilisp-bismongtk~

--- guigtk/build.sh
+++ guigtk/build.sh
@@ -3,6 +3,7 @@
 BISMON_SHGIT1=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | head -c16)
 BISMON_SHGIT2=$(if git status | grep -q 'nothing to commit'; then echo; else echo +; fi)
 export BISMON_SHGIT=$(printf %s%s $BISMON_SHGIT1 $BISMON_SHGIT2)
+/bin/mv minilisp-bismongtk minilisp-bismongtk~
 gcc -Wall -Wextra -g -Og minilisp.c gtkbismonlisp.c jsonbismonlisp.c -DBISMON_GIT=\"$BISMON_SHGIT\" \
     -I . \
     -I /usr/local/include $(pkg-config --cflags gtk+-3.0 jansson) \
