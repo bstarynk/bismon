4dd28ba4de2060a1086c17ed49471f3a1b2c9458
Basile Starynkevitch <basile@starynkevitch.net>
2022-03-03 11:19:57 +0100

    using shell variable BISMON_OPTIM

--- guigtk/build.sh
+++ guigtk/build.sh
@@ -4,9 +4,14 @@ BISMON_SHGIT1=$(git log --format=oneline -q -1 | cut '-d '  -f1 | tr -d '\n' | h
 BISMON_SHGIT2=$(if git status | grep -q 'nothing to commit'; then echo; else echo +; fi)
 export BISMON_SHGIT=$(printf %s%s $BISMON_SHGIT1 $BISMON_SHGIT2)
 /bin/mv minilisp-bismongtk minilisp-bismongtk~
+
+if [ -z "$BISMON_OPTIM" ]; then
+    BISMON_OPTIM="-O1"
+fi
+
 errbase=0
 for cfile in minilisp.c gtkbismonlisp.c jsonbismonlisp.c ; do
-    gcc -Wall -Wextra -g -Og -c $cfile -DBISMON_GIT=\"$BISMON_SHGIT\" \
+    gcc -Wall -Wextra -g $BISMON_OPTIM -c $cfile -DBISMON_GIT=\"$BISMON_SHGIT\" \
 	-DERROR_BASE_NUM=$errbase \
     -I . \
     -I /usr/local/include $(pkg-config --cflags gtk+-3.0 jansson) \
@@ -17,6 +22,6 @@ for cfile in minilisp.c gtkbismonlisp.c jsonbismonlisp.c ; do
     errbase=$[errbase+1000]
 done
 
-gcc -Wall -Wextra -g -Og minilisp.o gtkbismonlisp.o jsonbismonlisp.o \
+gcc -Wall -Wextra -g $BISMON_OPTIM minilisp.o gtkbismonlisp.o jsonbismonlisp.o \
     -L /usr/local/lib $(pkg-config --libs gtk+-3.0 jansson) \
     -o minilisp-bismongtk
