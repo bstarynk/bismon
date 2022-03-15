a84c0509502a7d2c6d0a9ba5ddf0ad10c150bccf
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-03-15 14:34:36 +0100

    improve comment in test02.sh

--- guigtk/test02.sh
+++ guigtk/test02.sh
@@ -3,6 +3,10 @@
 ## see https://github.com/bstarynk/bismon/
 ;;;+++
 
+;;; consider building with:
+;;;         env BISMON_OPTIM='-O0 -g' ./build.sh
+;;; then debugging with:
+;;;         gdb --args ./minilisp-bismongtk --verbose --script test02.sh
 ;
 
 #"ABC(xxy\tk)ABC"  34
