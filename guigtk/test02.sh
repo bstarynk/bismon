cc7d532b558cfcd954c744367fdcb76a56f0bb9d
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-03-17 11:30:19 +0100

    test02.sh is testing the gtk builder

--- guigtk/test02.sh
+++ guigtk/test02.sh
@@ -9,7 +9,40 @@
 ;;;         gdb --args ./minilisp-bismongtk --verbose --script test02.sh
 ;
 
-#"ABC(xxy\tk)ABC"  34
+
+(define mybuilder ())
+
+
+(setq mybuilder
+ (gtk_builder
+
+#"ABC(
+  <interface>
+  <object class="GtkDialog" id="dialog1">
+    <child internal-child="vbox">
+      <object class="GtkBox" id="vbox1">
+        <property name="border-width">10</property>
+        <child internal-child="action_area">
+          <object class="GtkButtonBox" id="hbuttonbox1">
+            <property name="border-width">20</property>
+            <child>
+              <object class="GtkButton" id="ok_button">
+                <property name="label">gtk-ok</property>
+                <property name="use-stock">TRUE</property>
+                <!-- signal name="clicked" handler="ok_button_clicked" -->
+              </object>
+            </child>
+          </object>
+        </child>
+      </object>
+    </child>
+  </object>
+</interface>
+)ABC"
+ )
+)
+
+
 
 (gtk_loop)
 
