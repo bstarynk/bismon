a3ee97c7d9d63cec77d395c794fa3ef96974e8c5
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-07-05 12:24:10 +0200

    update copyright year

--- dump_BM.c
+++ dump_BM.c
@@ -3,7 +3,7 @@
 
 /***
     BISMON 
-    Copyright © 2018 - 2021 CEA (Commissariat à l'énergie atomique et aux énergies alternatives)
+    Copyright © 2018 - 2022 CEA (Commissariat à l'énergie atomique et aux énergies alternatives)
     contributed by Basile Starynkevitch (working at CEA, LIST, France)
     <basile@starynkevitch.net> or <basile.starynkevitch@cea.fr>
 
