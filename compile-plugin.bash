673bea541162a895727f92b4c48c9be3d77116dc
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-07-08 14:28:53 +0200

    verbose compile-plugin.bash, probably buggy

--- compile-plugin.bash
+++ compile-plugin.bash
@@ -2,3 +2,7 @@
 ## SPDX-License-Identifier: GPL-3.0-or-later
 ## See https://github.com/bstarynk/bismon/
 ## file compile-plugin.bash
+PLUGINSOURCE=$1
+PLUGINSO=$(dirname $1)/$(basename $1 .c).so
+printf "%s: compiling Bismon plugin source %s to %s with compiler %s and flags %s in %s\n" $0 $PLUGINSOURCE $PLUGINSO "$BISMONMK_HOST_CC" "$BISMON_CFLAGS" $(/bin/pwd)
+$BISMONMK_HOST_CC -fPIC -shared $BISMON_CFLAGS $PLUGINSOURCE -o $PLUGINSO
