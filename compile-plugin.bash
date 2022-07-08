2220cbc9f126a546dd8dc0a187b3f5d6851b499d
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-07-08 15:05:15 +0200

    working compile-plugin.bash for Pliugins/SayHello.c

--- compile-plugin.bash
+++ compile-plugin.bash
@@ -5,4 +5,5 @@
 PLUGINSOURCE=$1
 PLUGINSO=$(dirname $1)/$(basename $1 .c).so
 printf "%s: compiling Bismon plugin source %s to %s with compiler %s and flags %s in %s\n" $0 $PLUGINSOURCE $PLUGINSO "$BISMONMK_HOST_CC" "$BISMON_CFLAGS" $(/bin/pwd)
-$BISMONMK_HOST_CC -fPIC -I$(/bin/pwd) -shared $BISMON_CFLAGS $PLUGINSOURCE -o $PLUGINSO
+#printenv
+$BISMONMK_HOST_CC -fPIC -I$(/bin/pwd)  -DBISMON_SHORTGIT=\"$BISMON_SHORT_GIT\" -shared $BISMON_CFLAGS  $(/usr/bin/pkg-config --cflags $BISMON_PACKAGES)  $PLUGINSOURCE -o $PLUGINSO
