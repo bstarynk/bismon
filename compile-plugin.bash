cdb88bf38e7bb7df90edc64c343ddc30070a79e5
Basile Starynkevitch <basile.starynkevitch@cea.fr>
2022-07-08 14:39:28 +0200

    better compile-plugin.bash, still buggy because of packages

--- compile-plugin.bash
+++ compile-plugin.bash
@@ -5,4 +5,4 @@
 PLUGINSOURCE=$1
 PLUGINSO=$(dirname $1)/$(basename $1 .c).so
 printf "%s: compiling Bismon plugin source %s to %s with compiler %s and flags %s in %s\n" $0 $PLUGINSOURCE $PLUGINSO "$BISMONMK_HOST_CC" "$BISMON_CFLAGS" $(/bin/pwd)
-$BISMONMK_HOST_CC -fPIC -shared $BISMON_CFLAGS $PLUGINSOURCE -o $PLUGINSO
+$BISMONMK_HOST_CC -fPIC -I$(/bin/pwd) -shared $BISMON_CFLAGS $PLUGINSOURCE -o $PLUGINSO
